500a22a72c84ea70163ed49af128857cecd6e0cc
PblCbGomel <yevgeniy.shevtsov.pblcb@mail.ru>
2022-06-20 12:22:35 +0300

    feat: added themes

---
+++ scripts/theme-script.js
@@ -0,0 +1,34 @@
+const ligthBtn = document.querySelector(".ligth-theme");
+const darkBtn = document.querySelector(".dark-theme");
+const calculatorWrapper = document.querySelector(".calculator-wrapper");
+const actionWrapper = document.querySelector(".actions-wrapper");
+const inputWrapper = document.querySelector(".input");
+const btnsArray = document.querySelectorAll(".btn");
+
+darkBtn.addEventListener("click", () => {
+  calculatorWrapper.classList.remove("calculator-wrapper-ligth");
+  calculatorWrapper.classList.add("calculator-wrapper-dark");
+  actionWrapper.classList.remove("actions-wrapper-ligth");
+  actionWrapper.classList.add("actions-wrapper-dark");
+  inputWrapper.classList.remove("input-ligth");
+  inputWrapper.classList.add("input-dark");
+
+  btnsArray.forEach((btn) => {
+    btn.classList.remove("btn-ligth");
+    btn.classList.add("btn-dark");
+  });
+});
+
+ligthBtn.addEventListener("click", () => {
+  calculatorWrapper.classList.add("calculator-wrapper-ligth");
+  calculatorWrapper.classList.remove("calculator-wrapper-dark");
+  actionWrapper.classList.add("actions-wrapper-ligth");
+  actionWrapper.classList.remove("actions-wrapper-dark");
+  inputWrapper.classList.add("input-ligth");
+  inputWrapper.classList.remove("input-dark");
+
+  btnsArray.forEach((btn) => {
+    btn.classList.remove("btn-dark");
+    btn.classList.add("btn-ligth");
+  });
+});
